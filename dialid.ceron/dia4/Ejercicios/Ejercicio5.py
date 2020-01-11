4e1cd3c642282a29141461328ae31efc42d5d4fa
Dialid <dialid.ceron@gmail.com>
2020-01-10 19:17:01 -0600

    Ejercicio5 terminado

--- dialid.ceron/dia4/Ejercicios/Ejercicio5.py
+++ dialid.ceron/dia4/Ejercicios/Ejercicio5.py
@@ -2,7 +2,7 @@
 #Cerón Rodríguez Lezly Dialid
 import argparse
 
-def eliminar_espacios(sucio, limpio, upper):
+def eliminar_espacios(sucio, limpio, upper,lower):
 	f1=open(sucio, 'r')
 	f2=open(limpio, 'w')
 	for line in f1:
@@ -11,10 +11,35 @@ def eliminar_espacios(sucio, limpio, upper):
 		for elemento in palabras:
 			if upper:
 				f2.write(elemento.upper()+' ')
+			elif lower:
+				f2.write(elemento.lower()+' ')
 			else:
 				f2.write(elemento+' ')
 		f2.write('\n')
 	f1.close()
 	f2.close()
 
-eliminar_espacios('texto_sucio.txt', 'texto_limpio.txt', True)
+#eliminar_espacios('texto_sucio.txt', 'texto_limpio.txt', False)
+
+def parse():
+
+    parser = argparse.ArgumentParser(   prog = 'argparse_creando_parser',
+                                        description = 'Programa limpiador de espacios en texto',
+                                        epilog='desarrollado por Dialid'   )
+
+    parser.add_argument('--in-file', action='store', default='texto_sucio.txt', type=str, required=False, help='Archivo a limpiar', metavar= 'ARCHIVOIN', dest='archivoin')
+    parser.add_argument('--out-file', action='store', default='texto_limpio.txt', type=str, required=False, help='Archivo sin espacios', metavar='ARCHIVOOUT', dest='archivoout')
+    parser.add_argument('--upper', action='store_true', help='imprimir en mayuscula', dest='upper')
+    parser.add_argument('--lower', action='store_true', help='imprimir en minuscula', dest='lower')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    
+    arguments = parse()
+    
+    if (arguments.upper and arguments.lower):
+    	print("Error, el archivo no se puede escribir en mayusculas y minusculas a la vez")
+        #print('Hola {} tienes {} años'.format(arguments.nombre.upper(), arguments.edad))
+    else:
+        eliminar_espacios(arguments.archivoin, arguments.archivoout, arguments.upper, arguments.lower)
